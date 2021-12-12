07811974d10dc9309ad0fde883ad76ca7ef8c729
dzh0n <plotnikov.evgeniy@gmail.com>
2021-12-12 16:48:07 +0500

    about page

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+var certificatesSlider;
+
+$(document).ready(function () {
+    certificatesSlider = new Swiper('.certificates-slider', {
+        spaceBetween: 45,
+        loop: false,
+        slidesPerView: 3,
+    });
+});

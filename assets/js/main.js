b4d289428a5d28d3c87d84b046d707f88f02ca4e
dzh0n <plotnikov.evgeniy@gmail.com>
2021-12-12 18:29:10 +0500

    specialists

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,7 @@
 var certificatesSlider;
 
 $(document).ready(function () {
+    $('select').niceSelect();
     certificatesSlider = new Swiper('.certificates-slider', {
         spaceBetween: 45,
         loop: false,

c44b6366a125cfface784e93f31c05175036b59c
dzh0n <plotnikov.evgeniy@gmail.com>
2021-12-13 16:27:17 +0500

    last

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,4 +7,88 @@ $(document).ready(function () {
         loop: false,
         slidesPerView: 3,
     });
+
+    var specialistsSlider = new Swiper('#specialists-slider', {
+        spaceBetween: 30,
+        loop: false,
+        slidesPerView: 4,
+        navigation: {
+            prevEl: '.specialists-slider-navs .swiper-button-prev',
+            nextEl: '.specialists-slider-navs .swiper-button-next',
+        },
+    });
+
+    var helpSlider = new Swiper('#help-slider', {
+        spaceBetween: 30,
+        loop: false,
+        slidesPerView: 4,
+        navigation: {
+            prevEl: '.help-slider-navs .swiper-button-prev',
+            nextEl: '.help-slider-navs .swiper-button-next',
+        },
+    });
+
+    var newsSlider = new Swiper('#news-slider', {
+        spaceBetween: 30,
+        loop: false,
+        slidesPerView: 4,
+        navigation: {
+            prevEl: '.news-slider-navs .swiper-button-prev',
+            nextEl: '.news-slider-navs .swiper-button-next',
+        },
+    });
+
+    var ourServicessSlider = new Swiper('#our-services-slider', {
+        spaceBetween: 30,
+        loop: false,
+        slidesPerView: 4,
+        navigation: {
+            prevEl: '.our-services-slider-navs .swiper-button-prev',
+            nextEl: '.our-services-slider-navs .swiper-button-next',
+        },
+    });
+
+    var aboutCenterSlider = new Swiper('#about-center-slider', {
+        spaceBetween: 30,
+        loop: false,
+        slidesPerView: 1,
+        pagination: {
+            el: '#about-center-slider .swiper-pagination',
+            type: 'bullets',
+        },
+        navigation: {
+            prevEl: '#about-center-slider .swiper-button-prev',
+            nextEl: '#about-center-slider .swiper-button-next',
+        },
+    });
+
+
+    var pageSlider = new Swiper('.page-slider', {
+        spaceBetween: 0,
+        loop: false,
+        slidesPerView: 1,
+        pagination: {
+            el: '.page-slider .swiper-pagination',
+            type: 'bullets',
+        },
+
+    });
+
+    var peopleSlider = new Swiper('.people-slider', {
+        spaceBetween: 0,
+        loop: false,
+        slidesPerView: 1,
+        pagination: {
+            el: '.people-slider .swiper-pagination',
+            type: 'custom',
+            renderCustom: function (swiper, current, total) {
+                return '<span>'+current+' / '+total+'</span>'
+            },
+        },
+        navigation: {
+            prevEl: '.people-slider .swiper-button-prev',
+            nextEl: '.people-slider .swiper-button-next',
+        },
+
+    });
 });
